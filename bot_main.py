c51a58bb719c62e3a4025eee7d607f2505954da3
Av Il <avil48job@gmail.com>
2020-01-06 20:46:24 +0200

    added timezone support and bugfixes[issue #13]

--- bot_main.py
+++ bot_main.py
@@ -220,5 +220,3 @@ if __name__ == "__main__":
     scheduler.add_job(func=time_schedule, trigger="interval", seconds=60)
     scheduler.start()
     server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
-    bot.remove_webhook()
-    bot.infinity_polling()
